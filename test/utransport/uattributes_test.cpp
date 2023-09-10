57a78389f6f1eaa035cb7abe57cee445e113ef6f
MelamudMichael <melamudmichael@gmail.com>
2023-09-10 07:54:45 +0000

    adding unit test

--- test/utransport/uattributes_test.cpp
+++ test/utransport/uattributes_test.cpp
@@ -4,6 +4,66 @@
 
 using namespace uprotocol::utransport::datamodel;
 
+
+
+#include <gtest/gtest.h>
+#include "UAttributes.h"  // Include the header file where UAttributes and UAttributesBuilder are defined
+
+// Test the UAttributes class
+TEST(UAttributesTest, Class) 
+{
+    // Create an empty UAttributes object
+    uprotocol::utransport::datamodel::UAttributes attributes;
+    
+    // Test the isEmpty() method for an empty object
+    EXPECT_TRUE(attributes.isEmpty());
+
+    st_uuid id;
+
+    if (false == uuid_v6_generate(&id))
+    {
+        spdlog::error("uuid_v6_generate_str failed");
+        return -1;
+    }
+
+    // Create a UAttributes object with some values
+    uprotocol::utransport::datamodel::UMessageType type = uprotocol::utransport::datamodel::UMessageType::PUBLISH;
+    uprotocol::utransport::datamodel::UPriority priority = uprotocol::utransport::datamodel::UPriority::STANDARD;
+    uprotocol::utransport::datamodel::UAttributesBuilder builder(id, type, priority);
+    builder.withTtl(100);
+    builder.withToken("sample_token");
+    builder.withHint(uprotocol::utransport::datamodel::USerializationHint::JSON);
+    builder.withSink(UUri(uri_datamodel::uri_authority::local(), 
+                        uri_datamodel::uri_entity::fromName("body.access"),
+                        uri_datamodel::uri_resource::fromName("door")));
+    builder.withPermissionLevel(5);
+    builder.withCommStatus(200);
+    builder.withReqId(id);
+    uprotocol::utransport::datamodel::UAttributes nonEmptyAttributes = builder.build();
+
+    // Test the isEmpty() method for a non-empty object
+    EXPECT_FALSE(nonEmptyAttributes.isEmpty());
+
+    // Test getters for the attributes
+    EXPECT_EQ(nonEmptyAttributes.id(), id);
+    EXPECT_EQ(nonEmptyAttributes.type(), type);
+    EXPECT_EQ(nonEmptyAttributes.priority(), priority);
+    EXPECT_EQ(nonEmptyAttributes.ttl().value_or(0), 100);
+    EXPECT_EQ(nonEmptyAttributes.token().value_or(""), "sample_token");
+    EXPECT_EQ(nonEmptyAttributes.serializationHint().value(), uprotocol::utransport::datamodel::USerializationHint::JSON);
+    EXPECT_EQ(nonEmptyAttributes.sink().value().toString(), "http://example.com");
+    EXPECT_EQ(nonEmptyAttributes.permissionLevel().value_or(0), 5);
+    EXPECT_EQ(nonEmptyAttributes.commStatus().value_or(0), 200);
+    EXPECT_EQ(nonEmptyAttributes.reqId().value(),id);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+
 int main() 
 {
     st_uuid uuid;
