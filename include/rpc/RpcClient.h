487d78dad5b70396915e13c5e7f63ac68a1e46d8
MelamudMichael <melamudmichael@gmail.com>
2023-10-09 18:45:47 +0000

    compilation fix

--- include/rpc/RpcClient.h
+++ include/rpc/RpcClient.h
@@ -2,26 +2,30 @@
 #define _RPC_CLIENT_H_
 
 #include <future>
+#include <include/utransport/datamodel/UPayload.h>
+#include <include/utransport/datamodel/UAttributes.h>
+#include <include/utransport/datamodel/UStatus.h>
 
+using namespace uprotocol::utransport;
 using namespace uprotocol::utransport::datamodel;
 using namespace uri_datamodel;
 
 class RpcClient 
 {
-
-    /**
-     * Support for RPC method invocation.
-     * @param topic topic of the method to be invoked (i.e. the name of the API we are calling).
-     * @param payload The request message to be sent to the server.
-     * @param attributes Metadata for the method invocation (i.e. priority, timeout, etc.)
-     * @return Returns the CompletableFuture with the result or exception.
-     */
-    virtual std::future<UPayload> invokeMethod(
-        UUri topic, 
-        UPayload payload, 
-        UAttributes attributes) = 0;
-    
-     virtual ~RpcClient() {} 
-}
+    public:
+        /**
+        * Support for RPC method invocation.
+        * @param topic topic of the method to be invoked (i.e. the name of the API we are calling).
+        * @param payload The request message to be sent to the server.
+        * @param attributes Metadata for the method invocation (i.e. priority, timeout, etc.)
+        * @return Returns the CompletableFuture with the result or exception.
+        */
+        virtual std::future<UPayload> invokeMethod(
+            UUri topic, 
+            UPayload payload, 
+            UAttributes attributes) = 0;
+        
+        virtual ~RpcClient() {} 
+};
 
 #endif /*_RPC_CLIENT_H_*/
