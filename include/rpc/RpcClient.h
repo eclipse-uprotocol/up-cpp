2d9e19cd5512563964782f8009e1080f02361f60
MelamudMichael <melamudmichael@gmail.com>
2023-10-12 12:53:21 +0000

    rpc client definitino

--- include/rpc/RpcClient.h
+++ include/rpc/RpcClient.h
@@ -21,9 +21,9 @@ class RpcClient
         * @return Returns the CompletableFuture with the result or exception.
         */
         virtual std::future<UPayload> invokeMethod(
-            UUri topic, 
-            UPayload payload, 
-            UAttributes attributes) = 0;
+            UUri &topic, 
+            UPayload &payload, 
+            UAttributes &attributes) = 0;
         
         virtual ~RpcClient() {} 
 };
