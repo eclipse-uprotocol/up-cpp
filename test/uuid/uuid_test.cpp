3b35292e1afe4709540f7eb2d68bad8099510338
Nagesh Bandla <nageswararao.bandla@gm.com>
2023-11-03 17:57:58 -0400

    Update to unit tests

--- test/uuid/uuid_test.cpp
+++ test/uuid/uuid_test.cpp
@@ -18,12 +18,47 @@ TEST(UUIDTest, Class)
     EXPECT_EQ(uuIdNew.toString(), str);
 }
 
-TEST(UUIDTest, Negative)
+TEST(UUIDTest, NegStringConstructor)
 {
     std::string str = "0080b636-8303-8701-8ebe-7a9a9e767a9f";
     UUID uuIdNew(str);
     str = "test" +str;
     EXPECT_NE(uuIdNew.toString(), str);
+}
+
+TEST(UUIDTest, NegEmptyString)
+{
+    //Empty String
+    std::string str = "";
+    UUID uuId(str);
+    EXPECT_NE(uuId.toString(), str);
+    EXPECT_EQ(uuId.getCount(), uint64_t(0));
+}
+
+TEST(UUIDTest, NegEmptyByteArray)
+{
+    std::array<uint8_t, 16> buff{};
+    UUID uuIdFromByteArr(buff.data());
+    uint64_t val(0);
+
+    EXPECT_EQ(uuIdFromByteArr.getCount(), val);
+}
+
+TEST(UUIDTest, NegInvalidByteArray)
+{
+    std::array<uint8_t, 16> buff{};
+    uint64_t msb(1);
+    uint64_t lsb(1);
+    for (int i = 0; i < 8; i++) {
+        buff[i] = msb ;
+        buff[i + 8] = lsb;
+    }
+    UUID uuIdFromByteArr(buff.data());
+    uint64_t val(1);
+    std::string str = "";
+
+    EXPECT_NE(uuIdFromByteArr.toString(), str);
+    EXPECT_NE(uuIdFromByteArr.getCount(), val);
 }
 
 int main(int argc, char **argv)
