c64687ce4b64755d986ed3ab805b069583ace84b
MelamudMichael <melamudmichael@gmail.com>
2024-02-06 08:00:58 +0000

    aliging to conan 2.0

--- conanfile.py
+++ conanfile.py
@@ -27,10 +27,10 @@ class UpCpp(ConanFile):
 
     def requirements(self):
         self.requires("protobuf/3.21.12")
-        self.requires("gtest/1.14.0")
+       # self.requires("gtest/1.14.0")
 
     def layout(self):
-        cmake_layout(self, src_folder="src")
+        cmake_layout(self, src_folder=".")
 
     def generate(self):
         tc = CMakeToolchain(self)
