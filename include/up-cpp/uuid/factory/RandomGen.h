7724601a00343cce3d0b0f4e933f79d25daae29a
Michael Melamud <melamudmichael@gmail.com>
2024-04-03 06:36:48 +0000

    reverting to 2023

--- include/up-cpp/uuid/factory/RandomGen.h
+++ include/up-cpp/uuid/factory/RandomGen.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 General Motors GTO LLC
+ * Copyright (c) 2023 General Motors GTO LLC
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -19,7 +19,7 @@
  * under the License.
  * 
  * SPDX-FileType: SOURCE
- * SPDX-FileCopyrightText: 2024 General Motors GTO LLC
+ * SPDX-FileCopyrightText: 2023 General Motors GTO LLC
  * SPDX-License-Identifier: Apache-2.0
  */
 
