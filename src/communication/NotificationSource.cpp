7809efc25faf65a911b0c6fc2d7963b8eb56fb54
ruchir <ruchir.chauhan@gm.com>
2024-06-21 15:07:48 -0400

    Notificationsource Implementation

--- src/communication/NotificationSource.cpp
+++ src/communication/NotificationSource.cpp
@@ -10,3 +10,37 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "up-cpp/communication/NotificationSource.h"
+
+namespace uprotocol::communication {
+using namespace uprotocol::datamodel::builder;
+
+NotificationSource::NotificationSource(
+    std::shared_ptr<transport::UTransport> transport, v1::UUri&& source,
+    v1::UUri&& sink, std::optional<v1::UPayloadFormat> payload_format,
+    std::optional<v1::UPriority> priority,
+    std::optional<std::chrono::milliseconds> ttl)
+    : transport_(std::move(transport)),
+      notify_builder_(
+          UMessageBuilder::notification(std::move(source), std::move(sink))
+              .withPriority(priority.value_or(v1::UPriority::UPRIORITY_CS1))) {
+	if (payload_format.has_value()) {
+		notify_builder_.withPayloadFormat(payload_format.value());
+	}
+
+	if (ttl.has_value()) {
+		notify_builder_.withTtl(ttl.value());
+	}
+}
+
+v1::UStatus NotificationSource::notify(
+    datamodel::builder::Payload&& payload) const {
+	auto message = notify_builder_.build(std::move(payload));
+	return transport_->send(std::move(message));
+}
+
+v1::UStatus NotificationSource::notify() const {
+	auto message = notify_builder_.build();
+	return transport_->send(std::move(message));
+}
+
+}  // namespace uprotocol::communication
