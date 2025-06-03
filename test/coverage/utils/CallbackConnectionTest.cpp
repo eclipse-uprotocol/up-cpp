23d97d7af91ea5af03578a6920e4b25592036181
Pavlo Kleymonov <pkleymonov@blackberry.com>
2025-06-03 18:37:46 +0200

    fix CallbackTest.ReturnValuesAreMoved
    std::string.reserve(capacity)
    Effects: After reserve(), capacity() is greater or equal to the argument of reserve.
    
    C++ International Standard 2014 (N3797) 21.4.4 basic_string capacity

--- test/coverage/utils/CallbackConnectionTest.cpp
+++ test/coverage/utils/CallbackConnectionTest.cpp
@@ -446,7 +446,7 @@ TEST_F(CallbackTest, ReturnValuesAreMoved) {
 		auto result = callable();
 		EXPECT_TRUE(result);
 		EXPECT_EQ(original_string_location, (*result).data());
-		EXPECT_EQ(expected_capacity, (*result).capacity());
+		EXPECT_LE(expected_capacity, (*result).capacity());
 		// Just to be safe, check our assumptions about copies vs moves. The
 		// a_copy variable should hold a copy of the original string, this time
 		// with a different pointer and capacity.
